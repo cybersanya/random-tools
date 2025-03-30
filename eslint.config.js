c1ef29cbbb0b5f684ebd9bf0060177fc7acbe71a
Alexander <a.s.romashkin@yandex.ru>
2025-03-30 17:52:06 +0300

    chore: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+  pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  eslintConfigPrettier,
+])
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Textarea } from 'primevue'
+import { ref } from 'vue'
+
+const optionsAsText = ref('')
+</script>
+
+<template>
+  <Textarea v-model="optionsAsText" rows="10" auto-resize />
+</template>
+
+<style>
+body {
+  font-family:
+    system-ui,
+    -apple-system,
+    Roboto,
+    sans-serif;
+}
+</style>
